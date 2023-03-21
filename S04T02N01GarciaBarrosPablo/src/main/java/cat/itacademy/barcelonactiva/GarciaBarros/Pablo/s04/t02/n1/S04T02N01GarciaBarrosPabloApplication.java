1a0abedb273d1b2925a8dd14f457a8ae53b6e1ef
pgarcia86 <pgarciabarros86@gmail.com>
2023-03-21 16:59:47 +0100

    Ejercicio terminado

--- S04T02N01GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t02/n1/S04T02N01GarciaBarrosPabloApplication.java
+++ S04T02N01GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t02/n1/S04T02N01GarciaBarrosPabloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S04T02GarciaBarrosPabloApplication {
+public class S04T02N01GarciaBarrosPabloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(S04T02GarciaBarrosPabloApplication.class, args);
+		SpringApplication.run(S04T02N01GarciaBarrosPabloApplication.class, args);
 	}
 
 }
--- S04T02N01GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t02/n1/controllers/FrutaController.java
+++ S04T02N01GarciaBarrosPablo/src/main/java/cat/itacademy/barcelonactiva/GarciaBarros/Pablo/s04/t02/n1/controllers/FrutaController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import cat.itacademy.barcelonactiva.GarciaBarros.Pablo.s04.t02.n1.model.repository.Fruta;
 import cat.itacademy.barcelonactiva.GarciaBarros.Pablo.s04.t02.n1.model.services.FrutaService;
@@ -16,7 +15,7 @@ import cat.itacademy.barcelonactiva.GarciaBarros.Pablo.s04.t02.n1.model.services
 
 @Controller
 //Con la notación controller establezco que esta clase es un controller
-public class FruitController {
+public class FrutaController {
 	
 	@Autowired //Esta notación dice que debe instanciar un objeto de la siguiente instancia
 	private FrutaService frutas;
